06b740869173538e3f35166e16e5c1598dffb764
NickoTesla <mykolandwirt@gmail.com>
2023-03-21 09:08:04 +0100

    Update bdlist.py

--- bdlist.py
+++ bdlist.py
@@ -13,29 +13,28 @@ users = [
 def get_birthdays_per_week(users):
     today = datetime.date.today()
     weekdays = ['Monday', 'Tuesday', 'Wednesday',
-                'Thursday', 'Friday', 'Saturday', 'Sunday']
+                'Thursday', 'Friday']
+
     birthday_week = [today + datetime.timedelta(days=i) for i in range(7)]
 
     birthdays = {weekday: [] for weekday in weekdays}
 
     for user in users:
         name = user['name']
-        birthday = user['birthday'].date()
+        birthday = user['birthday'].replace(year=today.year).date()
+
         if birthday in birthday_week:
-            index = birthday_week.index(birthday)
-            weekday = weekdays[index]
-            birthdays[weekday].append(name)
-        elif birthday.weekday() >= 5:  # Weekend birthday
-            weekdays_cycle = cycle(weekdays)
-            for i in range(7):
-                next_weekday = next(weekdays_cycle)
-                if next_weekday == 'Monday':
-                    continue
-                if i == 6:  # Couldn't find a weekday this week, so use next week's Monday
-                    next_weekday = 'Monday'
-                birthdays[next_weekday].append(name)
+
+            if birthday.weekday() in (5, 6):
+                birthdays['Monday'].append(name)
+            else:
+                birthdays[birthday.strftime("%A")].append(name)
 
     for weekday in weekdays:
         if birthdays[weekday]:
             names = ", ".join(birthdays[weekday])
             print(f"{weekday}: {names}")
+
+
+if __name__ == "__main__":
+    get_birthdays_per_week(users)
